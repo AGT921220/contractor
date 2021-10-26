9abb6017d73e879afbe4534d67b59112bacb0bd2
AGT921220 <alfredo.gutierrez.92@hotmail.com>
2021-10-26 17:51:09 -0500

    client Test

---
+++ tests/Feature/ClientTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Client;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ClientTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+     /**
+      * @test
+      */
+    public function canIndexClients()
+    {
+
+
+        dd(4);
+        // $response = $this->get('/');
+
+        // $response->assertStatus(200);
+//        dd(4);
+//        dd(count(Client::all()));
+    }
+}
