7b831907120051aa2441882682c0992be7e01c7c
AGT921220 <alfredo.gutierrez.92@hotmail.com>
2021-10-26 18:50:12 -0500

    tests

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -3,30 +3,33 @@
 namespace Tests\Feature;
 
 use App\Client;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class ClientTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-
+    public const DIRECTOR = 1;
      /**
       * @test
       */
     public function canIndexClients()
+    {   
+
+        $rootUser = User::find(self::DIRECTOR);
+        $this->actingAs($rootUser);
+
+
+         $response = $this->get('/users');
+         $response->getOriginalContent()->users;
+
+         $this->assertIndexClients($response);
+    }
+
+    private function assertIndexClients(TestResponse $response):void
     {
-
-
-        dd(4);
-        // $response = $this->get('/');
-
-        // $response->assertStatus(200);
-//        dd(4);
-//        dd(count(Client::all()));
+        $response->assertStatus(200);        
     }
 }
